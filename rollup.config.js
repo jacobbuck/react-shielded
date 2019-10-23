856670c791917689bb2e4761686d0613e32cd3c5
Jacob Buck <buck.jacob@gmail.com>
2019-10-23 20:41:11 +1300

    Add build tools

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,8 @@
+import svgr from '@svgr/rollup';
+import autoprefixer from 'autoprefixer';
+import cssnano from 'cssnano';
 import babel from 'rollup-plugin-babel';
+import postcss from 'rollup-plugin-postcss';
 
 export default {
   input: 'src/index.js',
@@ -7,6 +11,14 @@ export default {
     file: 'lib/index.js',
     format: 'cjs',
   },
-  external: ['prop-types', 'react'],
-  plugins: [babel()],
+  external: ['react', 'react-dom'],
+  plugins: [
+    svgr(),
+    postcss({
+      extensions: ['.css'],
+      modules: true,
+      plugins: [autoprefixer(), cssnano()],
+    }),
+    babel(),
+  ],
 };
