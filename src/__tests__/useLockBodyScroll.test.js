0da3a4e7f880caa79109c3f5c4119db09c0fbcf3
Jacob Buck <buck.jacob@gmail.com>
2020-09-04 22:14:13 +0000

    Add useLockBodyScroll.test.js

---
+++ src/__tests__/useLockBodyScroll.test.js
@@ -0,0 +1,29 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import useLockBodyScroll from '../useLockBodyScroll';
+
+const TestComponent = () => {
+  useLockBodyScroll();
+  return <div />;
+};
+
+TestComponent.propTypes = {};
+
+test('sets "overflow: hidden" style on document body', () => {
+  render(<TestComponent />);
+  expect(document.body).toHaveStyle('overflow: hidden');
+});
+
+test('unsets "overflow: hidden" style on document body on unmount', () => {
+  const { unmount } = render(<TestComponent />);
+  unmount();
+  expect(document.body).not.toHaveStyle('overflow: hidden');
+});
+
+test('reverts "overflow" style on document body on unmount', () => {
+  document.body.style.overflow = 'scroll';
+  const { unmount } = render(<TestComponent />);
+  expect(document.body).toHaveStyle('overflow: hidden');
+  unmount();
+  expect(document.body).toHaveStyle('overflow: scroll');
+});
