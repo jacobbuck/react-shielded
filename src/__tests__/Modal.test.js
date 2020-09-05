20d931ef4d6afb410ce2682a553a3f3b0cf07158
Jacob Buck <buck.jacob@gmail.com>
2020-09-05 00:29:25 +0000

    Add Modal.test.js

---
+++ src/__tests__/Modal.test.js
@@ -0,0 +1,37 @@
+import { fireEvent, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import Modal from '../Modal';
+
+test('renders and matches snapshot', () => {
+  const { container } = render(<Modal onRequestClose={() => {}} />);
+  expect(container.firstChild).toMatchSnapshot();
+});
+
+test('iframe is focused on mount', () => {
+  const { getByLabelText } = render(<Modal onRequestClose={() => {}} />);
+  fireEvent.load(getByLabelText('The Shielded Site'));
+  expect(getByLabelText('The Shielded Site')).toHaveFocus();
+});
+
+test('loading indicator is hidden when iframe has loaded', () => {
+  const { getByLabelText, queryByLabelText } = render(
+    <Modal onRequestClose={() => {}} />
+  );
+  fireEvent.load(getByLabelText('The Shielded Site'));
+  expect(queryByLabelText('Loading')).toBeNull();
+});
+
+test('clicking close button calls onRequestClose callback', () => {
+  const close = jest.fn();
+  const { getByLabelText } = render(<Modal onRequestClose={close} />);
+  userEvent.click(getByLabelText('Close'));
+  expect(close).toHaveBeenCalledTimes(1);
+});
+
+test('pressing Escape key calls onRequestClose callback', () => {
+  const close = jest.fn();
+  const { getByRole } = render(<Modal onRequestClose={close} />);
+  userEvent.type(getByRole('dialog'), '{esc}');
+  expect(close).toHaveBeenCalledTimes(1);
+});
