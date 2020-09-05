7799addc492c779e91dfed6582e91d96b75b40cf
Jacob Buck <buck.jacob@gmail.com>
2020-09-05 01:50:25 +0000

    Removed unused dependency

--- src/Modal.js
+++ src/Modal.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import DocumentPortal from 'react-document-portal';
 import useKeypress from 'react-use-keypress';
 import Close from './close.svg';
